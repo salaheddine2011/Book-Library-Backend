083f2cf2af3d666534afbe295d8a6cdca1ec5af9
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-04-01 23:44:22 +0000

    Refactor Code

--- src/main/java/com/benkhanous/springbootlibrary/controller/BookController.java
+++ src/main/java/com/benkhanous/springbootlibrary/controller/BookController.java
@@ -4,6 +4,7 @@ import com.benkhanous.springbootlibrary.entity.Book;
 import com.benkhanous.springbootlibrary.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import utils.ExtractJWT;
 
 @CrossOrigin("http://localhost:3000")
 @RestController
@@ -18,18 +19,18 @@ public class BookController {
 
     @GetMapping("/secure/currentloans/count")
      public int currentLoansCount(@RequestHeader(value = "Authorization") String token){
-        String userEmail="anasnino@gmail.com";
+        String userEmail=ExtractJWT.payloadJWTExtraction(token,"\"sub\"");
         return bookService.currentLoansCount(userEmail);
     }
 
     @PutMapping("/secure/checkout")
     public Book checkoutBook(@RequestParam Long bookId,@RequestHeader(value = "Authorization") String token) throws Exception {
-     String userEmail="anasnino@gmail.com";
+     String userEmail= ExtractJWT.payloadJWTExtraction(token,"\"sub\"");
      return bookService.checkoutBook(userEmail,bookId);
     }
     @GetMapping("/secure/ischeckout/byuser")
     public Boolean checkoutBookByUser(@RequestParam Long bookId,@RequestHeader(value = "Authorization") String token) throws Exception{
-        String userEmail="anasnino@gmail.com";
+        String userEmail=ExtractJWT.payloadJWTExtraction(token,"\"sub\"");
         return bookService.checkoutBookByUser(userEmail,bookId);
     }
 
--- src/main/java/utils/ExtractJWT.java
+++ src/main/java/utils/ExtractJWT.java
@@ -6,7 +6,7 @@ import java.util.Map;
 
 public class ExtractJWT {
 
-    public static String payloadJWTExtraction(String token){
+    public static String payloadJWTExtraction(String token,String extraction){
         token.replace("Bearer","");
         String[] chunks=token.split("\\.");
         Base64.Decoder decoder=Base64.getUrlDecoder();
@@ -16,7 +16,7 @@ public class ExtractJWT {
         Map<String,String> map=new HashMap<>();
         for(String entry: entries){
             String[] keyValue=entry.split(":");
-            if (keyValue[0].equals("\"sub\"")) {
+            if (keyValue[0].equals(extraction)) {
                 int remove = 1;
                 if (keyValue[1].endsWith("}")) {
                     remove = 2;
@@ -26,8 +26,8 @@ public class ExtractJWT {
                 map.put(keyValue[0], keyValue[1]);
             }
         }
-        if (map.containsKey("\"sub\"")){
-            return map.get("\"sub\"");
+        if (map.containsKey(extraction)){
+            return map.get(extraction);
         }
         return null;
     }
