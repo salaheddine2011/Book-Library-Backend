ded934e198ab4f5f49d39b28e20fac33d7e7048a
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-07-22 22:16:58 +0100

    Library Services (Messages)

--- src/main/java/com/benkhanous/springbootlibrary/dao/MessageRepository.java
+++ src/main/java/com/benkhanous/springbootlibrary/dao/MessageRepository.java
@@ -1,7 +1,12 @@
 package com.benkhanous.springbootlibrary.dao;
 
 import com.benkhanous.springbootlibrary.entity.Message;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestParam;
 
 public interface MessageRepository extends JpaRepository<Message,Long> {
+    Page<Message> findByUserEmail(@RequestParam("user_email") String userEmail, Pageable pageable);
 }
