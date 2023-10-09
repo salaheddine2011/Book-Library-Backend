dd4f7faeae4320a176314165b97768bc5f59d12e
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-10-09 21:24:35 +0200

    Adding message service

---
+++ src/main/java/com/benkhanous/springbootlibrary/requestmodels/AdminQuestionRequest.java
@@ -0,0 +1,10 @@
+package com.benkhanous.springbootlibrary.requestmodels;
+
+import lombok.Data;
+
+@Data
+public class AdminQuestionRequest {
+    private Long id;
+
+    private String response;
+}
--- src/main/java/com/benkhanous/springbootlibrary/service/MessageService.java
+++ src/main/java/com/benkhanous/springbootlibrary/service/MessageService.java
@@ -2,10 +2,12 @@ package com.benkhanous.springbootlibrary.service;
 
 import com.benkhanous.springbootlibrary.dao.MessageRepository;
 import com.benkhanous.springbootlibrary.entity.Message;
+import com.benkhanous.springbootlibrary.requestmodels.AdminQuestionRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.Optional;
 
 @Service
 @Transactional
@@ -21,5 +23,16 @@ public class MessageService {
         Message message=new Message(messageRequest.getTitle(),messageRequest.getQuestion());
         message.setUserEmail(userEmail);
         messageRepository.save(message);
+    }
+
+    public void putMessage(AdminQuestionRequest adminQuestionRequest,String userEmail)throws Exception{
+        Optional<Message> message=messageRepository.findById(adminQuestionRequest.getId());
+        if(!message.isPresent()){
+            throw new Exception("Message not found");
+        }
+        message.get().setAdminEmail(userEmail);
+        message.get().setResponse(adminQuestionRequest.getResponse());
+        message.get().setClosed(true);
+        messageRepository.save(message.get());
     }
 }
