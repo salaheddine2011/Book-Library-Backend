91e8d06a527db163da2e1157fd26852d64137fb3
salaheddine benkhanous <benkhanoussalaheddine@gmail.com>
2023-11-10 11:30:33 +0100

    Book Service Enhancement (Paying)

--- src/main/java/com/benkhanous/springbootlibrary/service/BookService.java
+++ src/main/java/com/benkhanous/springbootlibrary/service/BookService.java
@@ -3,9 +3,11 @@ package com.benkhanous.springbootlibrary.service;
 import com.benkhanous.springbootlibrary.dao.BookRepository;
 import com.benkhanous.springbootlibrary.dao.CheckoutRepository;
 import com.benkhanous.springbootlibrary.dao.HistoryRepository;
+import com.benkhanous.springbootlibrary.dao.PaymentRepository;
 import com.benkhanous.springbootlibrary.entity.Book;
 import com.benkhanous.springbootlibrary.entity.Checkout;
 import com.benkhanous.springbootlibrary.entity.History;
+import com.benkhanous.springbootlibrary.entity.Payment;
 import com.benkhanous.springbootlibrary.responsemodels.ShelfCurrentLoansResponse;
 import org.hibernate.annotations.Check;
 import org.springframework.stereotype.Service;
@@ -25,10 +27,12 @@ public class BookService {
 
     private HistoryRepository historyRepository;
 
-    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository,HistoryRepository historyRepository) {
+    private PaymentRepository paymentRepository;
+    public BookService(BookRepository bookRepository, CheckoutRepository checkoutRepository,HistoryRepository historyRepository, PaymentRepository paymentRepository) {
         this.bookRepository = bookRepository;
         this.checkoutRepository = checkoutRepository;
         this.historyRepository=historyRepository;
+        this.paymentRepository=paymentRepository;
     }
 
     public Book checkoutBook(String userEmail, Long bookid) throws Exception {
@@ -36,6 +40,29 @@ public class BookService {
         Checkout validateCheckout = checkoutRepository.findByUserEmailAndBookId(userEmail, bookid);// for verify if the book is already checked
         if (!book.isPresent() || validateCheckout != null || book.get().getCopiesAvailable() <= 0) {
             throw new Exception("Book doesn't or is already checked by the user");
+        }
+        List<Checkout> currentBooksCheckout=checkoutRepository.findBooksByUserEmail(userEmail);
+        SimpleDateFormat sdf=new SimpleDateFormat("yyyy-MM-dd");
+        boolean bookNeedsReturned=false;
+        for(Checkout checkout : currentBooksCheckout){
+            Date d1=sdf.parse(checkout.getReturnDate());
+            Date d2=sdf.parse(LocalDate.now().toString());
+            TimeUnit time=TimeUnit.DAYS;
+            double differenceInTime=time.convert(d1.getTime()-d2.getTime(),TimeUnit.MILLISECONDS);
+            if(differenceInTime<0){
+                bookNeedsReturned=true;
+                break;
+            }
+        }
+        Payment userPayment = paymentRepository.findByUserEmail(userEmail);
+        if(userPayment !=null && userPayment.getAmount()>0 || (userPayment !=null && bookNeedsReturned ) ){
+                throw new Exception("Outstanding fees");
+        }
+        if(userPayment == null){
+            Payment payment=new Payment();
+            payment.setAmount(00.00);
+            payment.setUserEmail(userEmail);
+            paymentRepository.save(payment);
         }
         book.get().setCopiesAvailable(book.get().getCopiesAvailable() - 1);
         bookRepository.save(book.get());
